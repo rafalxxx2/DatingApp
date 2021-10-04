a24d725005622f590fd492f4da23fbaf31389d20
RafalSzczesniak <rafal501519@wp.pl>
2021-10-04 21:13:50 +0200

    End of section 8 - Start Repo pattern and AutMaper

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+using API.DTOs;
+using AutoMapper;
+using API.Entities;
+using System.Linq;
+using API.Extensions;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            IMappingExpression<AppUser, MemberDto> mappingExpression = CreateMap<AppUser, MemberDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBrith.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
